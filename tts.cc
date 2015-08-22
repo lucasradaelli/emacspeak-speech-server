523e7373c49ff59c7278556afb4455f1d906ff68
Juliano Ravasi <jravasi@google.com>
2015-08-22 20:38:14 -0300

    Replace declarations in tts.h with eci.h.

--- tts.cc
+++ tts.cc
@@ -14,26 +14,25 @@ using std::vector;
 
 namespace {
 
-void (*eciVersion)(char *);
-void *(*eciNewEx)(enum ECILanguageDialect);
-int (*eciGetAvailableLanguages)(enum ECILanguageDialect *, int *);
-void (*eciDelete)(void *);
-int (*eciReset)(void *);
-int (*eciStop)(void *);
-int (*eciClearInput)(void *);
-int (*eciPause)(void *, int);
-int (*eciSynthesize)(void *);
-int (*eciSynchronize)(void *);
-int (*eciSpeaking)(void *);
-int (*eciAddText)(void *, char *);
-int (*eciInsertIndex)(void *, int);
-int (*eciSetParam)(void *, int, int);
-int (*eciGetVoiceParam)(void *, int, int);
-int (*eciSetVoiceParam)(void *, int, int, int);
-int (*eciSetOutputBuffer)(void *, int, short *);
-int (*eciSetOutputDevice)(void *, int);
-void (*eciRegisterCallback)(void *,
-    ECICallbackReturn (*)(void *, ECIMessage, long, void *), void *);
+void (*eciVersion_)(char *);
+void *(*eciNewEx_)(ECILanguageDialect);
+int (*eciGetAvailableLanguages_)(ECILanguageDialect *, int *);
+void (*eciDelete_)(void *);
+int (*eciReset_)(void *);
+int (*eciStop_)(void *);
+int (*eciClearInput_)(void *);
+int (*eciPause_)(void *, int);
+int (*eciSynthesize_)(void *);
+int (*eciSynchronize_)(void *);
+int (*eciSpeaking_)(void *);
+int (*eciAddText_)(void *, char *);
+int (*eciInsertIndex_)(void *, int);
+int (*eciSetParam_)(void *, int, int);
+int (*eciGetVoiceParam_)(void *, int, int);
+int (*eciSetVoiceParam_)(void *, int, int, int);
+int (*eciSetOutputBuffer_)(void *, int, short *);
+int (*eciSetOutputDevice_)(void *, int);
+void (*eciRegisterCallback_)(void *, ECICallback, void *);
 
 }  // namespace
 
@@ -47,113 +46,105 @@ bool TTS::InitECI() {
     return false;
   }
 
-  eciVersion = (void (*)(char *))(unsigned long)dlsym(eciLib, "eciVersion");
-  eciGetAvailableLanguages = (int (*)(enum ECILanguageDialect *, int *))(
-      unsigned long)dlsym(eciLib, "eciGetAvailableLanguages");
-  eciNewEx = (void *(*)(enum ECILanguageDialect))(unsigned long)dlsym(
-      eciLib, "eciNewEx");
-  eciDelete = (void (*)(void *))(unsigned long)dlsym(eciLib, "eciDelete");
-  eciReset = (int (*)(void *))(unsigned long)dlsym(eciLib, "eciReset");
-  eciStop = (int (*)(void *))(unsigned long)dlsym(eciLib, "eciStop");
-  eciClearInput =
-      (int (*)(void *))(unsigned long)dlsym(eciLib, "eciClearInput");
-  eciPause = (int (*)(void *, int))(unsigned long)dlsym(eciLib, "eciPause");
-  eciSynthesize =
-      (int (*)(void *))(unsigned long)dlsym(eciLib, "eciSynthesize");
-  eciSynchronize =
-      (int (*)(void *))(unsigned long)dlsym(eciLib, "eciSynchronize");
-  eciSpeaking = (int (*)(void *))(unsigned long)dlsym(eciLib, "eciSpeaking");
-  eciInsertIndex =
-      (int (*)(void *, int))(unsigned long)dlsym(eciLib, "eciInsertIndex");
-  eciAddText =
-      (int (*)(void *, char *))(unsigned long)dlsym(eciLib, "eciAddText");
-  eciSetParam =
-      (int (*)(void *, int, int))(unsigned long)dlsym(eciLib, "eciSetParam");
-  eciGetVoiceParam = (int (*)(void *, int, int))(unsigned long)dlsym(
-      eciLib, "eciGetVoiceParam");
-  eciSetVoiceParam = (int (*)(void *, int, int, int))(unsigned long)dlsym(
-      eciLib, "eciSetVoiceParam");
-  eciRegisterCallback =
-      (void (*)(void *, ECICallbackReturn (*)(void *, ECIMessage, long, void *),
-                void *))(unsigned long)dlsym(eciLib, "eciRegisterCallback");
-  eciSetOutputBuffer = (int (*)(void *, int, short *))(unsigned long)dlsym(
-      eciLib, "eciSetOutputBuffer");
-  eciSetOutputDevice =
-      (int (*)(void *, int))(unsigned long)dlsym(eciLib, "eciSetOutputDevice");
+  eciVersion_ = (void (*)(char *))dlsym(eciLib, "eciVersion");
+  eciGetAvailableLanguages_ = (int (*)(ECILanguageDialect *, int *))dlsym(
+      eciLib, "eciGetAvailableLanguages");
+  eciNewEx_ = (void *(*)(ECILanguageDialect))dlsym(eciLib, "eciNewEx");
+  eciDelete_ = (void (*)(void *))dlsym(eciLib, "eciDelete");
+  eciReset_ = (int (*)(void *))dlsym(eciLib, "eciReset");
+  eciStop_ = (int (*)(void *))dlsym(eciLib, "eciStop");
+  eciClearInput_ = (int (*)(void *))dlsym(eciLib, "eciClearInput");
+  eciPause_ = (int (*)(void *, int))dlsym(eciLib, "eciPause");
+  eciSynthesize_ = (int (*)(void *))dlsym(eciLib, "eciSynthesize");
+  eciSynchronize_ = (int (*)(void *))dlsym(eciLib, "eciSynchronize");
+  eciSpeaking_ = (int (*)(void *))dlsym(eciLib, "eciSpeaking");
+  eciInsertIndex_ = (int (*)(void *, int))dlsym(eciLib, "eciInsertIndex");
+  eciAddText_ = (int (*)(void *, char *))dlsym(eciLib, "eciAddText");
+  eciSetParam_ = (int (*)(void *, int, int))dlsym(eciLib, "eciSetParam");
+  eciGetVoiceParam_ =
+      (int (*)(void *, int, int))dlsym(eciLib, "eciGetVoiceParam");
+  eciSetVoiceParam_ =
+      (int (*)(void *, int, int, int))dlsym(eciLib, "eciSetVoiceParam");
+  eciRegisterCallback_ = (void (*)(void *, ECICallback, void *))dlsym(
+      eciLib, "eciRegisterCallback");
+  eciSetOutputBuffer_ =
+      (int (*)(void *, int, short *))dlsym(eciLib, "eciSetOutputBuffer");
+  eciSetOutputDevice_ =
+      (int (*)(void *, int))dlsym(eciLib, "eciSetOutputDevice");
 
   // >
   // < check for needed symbols
 
   int okay = 1;
-  if (!eciNewEx) {
+  if (!eciNewEx_) {
     okay = 0;
     std::cerr << "eciNewEx undef\n";
   }
-  if (!eciDelete) {
+  if (!eciDelete_) {
     okay = 0;
     std::cerr << "eciDelete undef\n";
   }
-  if (!eciReset) {
+  if (!eciReset_) {
     okay = 0;
     std::cerr << "eciReset undef\n";
   }
-  if (!eciStop) {
+  if (!eciStop_) {
     okay = 0;
     std::cerr << "eciStop undef\n";
   }
-  if (!eciClearInput) {
+  if (!eciClearInput_) {
     okay = 0;
     std::cerr << "eciClearInput undef\n";
   }
-  if (!eciPause) {
+  if (!eciPause_) {
     okay = 0;
     std::cerr << "eciPause undef\n";
   }
-  if (!eciSynthesize) {
+  if (!eciSynthesize_) {
     okay = 0;
     std::cerr << "eciSynthesize undef\n";
   }
-  if (!eciSpeaking) {
+  if (!eciSpeaking_) {
     okay = 0;
     std::cerr << "eciSpeaking undef\n";
   }
-  if (!eciInsertIndex) {
+  if (!eciInsertIndex_) {
     okay = 0;
     std::cerr << "eciInsertIndex undef\n";
   }
-  if (!eciAddText) {
+  if (!eciAddText_) {
     okay = 0;
     std::cerr << "eciAddText undef\n";
   }
-  if (!eciSetParam) {
+  if (!eciSetParam_) {
     okay = 0;
     std::cerr << "eciSetParam undef\n";
   }
-  if (!eciSetParam) {
+  if (!eciSetParam_) {
     okay = 0;
     std::cerr << "eciSetParam undef\n";
   }
-  if (!eciGetVoiceParam) {
+  if (!eciGetVoiceParam_) {
     okay = 0;
     std::cerr << "eciGetVoiceParam undef\n";
   }
-  if (!eciSetVoiceParam) {
+  if (!eciSetVoiceParam_) {
     okay = 0;
     std::cerr << "eciSetVoiceParam undef\n";
   }
-  if (!eciRegisterCallback) {
+  if (!eciRegisterCallback_) {
     okay = 0;
     std::cerr << "eciRegisterCallback undef\n";
   }
-  if (!eciSetOutputBuffer) {
+  if (!eciSetOutputBuffer_) {
     okay = 0;
     std::cerr << "eciSetOutputBuffer undef\n";
   }
-  if (!eciSetOutputDevice) {
+  if (!eciSetOutputDevice_) {
     okay = 0;
     std::cerr << "eciSetOutputDevice undef\n";
   }
-  if (!eciGetAvailableLanguages) {
+  if (!eciGetAvailableLanguages_) {
     okay = 0;
     std::cerr << "eciGetAvailableLanguages undef\n";
   }
@@ -171,7 +162,7 @@ TTS::TTS(AlsaPlayer *alsa_player, const Options &options)
   int rc;
   ECILanguageDialect a_languages[LANG_INFO_MAX];
   int n_languages = LANG_INFO_MAX;
-  eciGetAvailableLanguages(a_languages, &n_languages);
+  eciGetAvailableLanguages_(a_languages, &n_languages);
 
   lang_switcher_.reset(new LangSwitcher(a_languages, n_languages));
 
@@ -181,20 +172,20 @@ TTS::TTS(AlsaPlayer *alsa_player, const Options &options)
     throw TTSError("No languages found\n");
   }
 
-  eci_handle_ = eciNewEx(a_default_language);
+  eci_handle_ = eciNewEx_(a_default_language);
   if (eci_handle_ == nullptr) {
     throw TTSError("Could not open text-to-speech engine.\n");
   }
 
   // Initialize TTS
-  if ((eciSetParam(eci_handle_, eciInputType, 1) == -1) ||
-      (eciSetParam(eci_handle_, eciSynthMode, 1) == -1) ||
-      (eciSetParam(eci_handle_, eciSampleRate, options.sample_rate) == -1)) {
-    eciDelete(eci_handle_);
+  if ((eciSetParam_(eci_handle_, eciInputType, 1) == -1) ||
+      (eciSetParam_(eci_handle_, eciSynthMode, 1) == -1) ||
+      (eciSetParam_(eci_handle_, eciSampleRate, options.sample_rate) == -1)) {
+    eciDelete_(eci_handle_);
     throw TTSError("Could not initialized tts");
   }
 
-  eciRegisterCallback(
+  eciRegisterCallback_(
       eci_handle_,
       +[](void *eci_handle, ECIMessage msg, long lparam, void *data) {
         TTS *tts = static_cast<TTS *>(data);
@@ -208,18 +199,18 @@ TTS::TTS(AlsaPlayer *alsa_player, const Options &options)
       this);
 
   // Set output to bufferl.
-  rc = eciSynchronize(eci_handle_);
+  rc = eciSynchronize_(eci_handle_);
   if (!rc) {
-    throw TTSError("Error  resetting TTS engine.\n");
+    throw TTSError("Error resetting TTS engine.\n");
   }
-  rc = eciSetOutputBuffer(eci_handle_, alsa_player_->period_size(),
-                          reinterpret_cast<short*>(alsa_player_->buffer()));
+  rc = eciSetOutputBuffer_(eci_handle_, alsa_player_->period_size(),
+                           reinterpret_cast<short *>(alsa_player_->buffer()));
   if (!rc) {
     throw TTSError("Error setting output buffer.\n");
   }
 }
 
-TTS::~TTS() { eciDelete(eci_handle_); }
+TTS::~TTS() { eciDelete_(eci_handle_); }
 
 int TTS::PlayTTS(const int count) {
   alsa_player_->Play(count);
@@ -227,7 +218,7 @@ int TTS::PlayTTS(const int count) {
 }
 
 bool TTS::IsSpeaking() {
-  if (eciSpeaking(eci_handle_)) {
+  if (eciSpeaking_(eci_handle_)) {
     return true;
   } else {
     return false;
@@ -235,7 +226,7 @@ bool TTS::IsSpeaking() {
 }
 
 bool TTS::Synchronize() {
-  int rc = eciSynchronize(eci_handle_);
+  int rc = eciSynchronize_(eci_handle_);
   if (rc) {
     return true;
   } else {
--- tts.h
+++ tts.h
@@ -2,6 +2,7 @@
 #define TTS_H_
 
 #include "alsa_player.h"
+#include "eci.h"
 
 #include <memory>
 #include <stdexcept>
@@ -12,82 +13,6 @@
 #define PACKAGEVERSION "1.0"
 #define ECILIBRARYNAME "libibmeci.so"
 
-/* The declarations  bellow  are derived from the publically
-   available eci.h (APIs for IBM Text To Speech) at http://ibmtts-sdk.sf.net
-
-   Its license is copied below
-
- * Copyright (c) 2005, 2006, IBM Corp. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.  *
- *     Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *     * Neither the name of IBM Corp. nor the names of its
- *     contributors may be used to endorse or promote products derived from
- *     this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
- * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
-enum ECILanguageDialect {
-  NODEFINEDCODESET = 0x00000000,
-  eciGeneralAmericanEnglish = 0x00010000,
-  eciBritishEnglish = 0x00010001,
-  eciCastilianSpanish = 0x00020000,
-  eciMexicanSpanish = 0x00020001,
-  eciStandardFrench = 0x00030000,
-  eciCanadianFrench = 0x00030001,
-  eciStandardGerman = 0x00040000,
-  eciStandardItalian = 0x00050000,
-  eciMandarinChinese = 0x00060000,
-  eciMandarinChineseGB = eciMandarinChinese,
-  eciMandarinChinesePinYin = 0x00060100,
-  eciMandarinChineseUCS = 0x00060800,
-  eciTaiwaneseMandarin = 0x00060001,
-  eciTaiwaneseMandarinBig5 = eciTaiwaneseMandarin,
-  eciTaiwaneseMandarinZhuYin = 0x00060101,
-  eciTaiwaneseMandarinPinYin = 0x00060201,
-  eciTaiwaneseMandarinUCS = 0x00060801,
-  eciBrazilianPortuguese = 0x00070000,
-  eciStandardJapanese = 0x00080000,
-  eciStandardJapaneseSJIS = eciStandardJapanese,
-  eciStandardJapaneseUCS = 0x00080800,
-  eciStandardFinnish = 0x00090000,
-  eciStandardKorean = 0x000A0000,
-  eciStandardKoreanUHC = eciStandardKorean,
-  eciStandardKoreanUCS = 0x000A0800,
-  eciStandardCantonese = 0x000B0000,
-  eciStandardCantoneseGB = eciStandardCantonese,
-  eciStandardCantoneseUCS = 0x000B0800,
-  eciHongKongCantonese = 0x000B0001,
-  eciHongKongCantoneseBig5 = eciHongKongCantonese,
-  eciHongKongCantoneseUCS = 0x000B0801,
-  eciStandardDutch = 0x000C0000,
-  eciStandardNorwegian = 0x000D0000,
-  eciStandardSwedish = 0x000E0000,
-  eciStandardDanish = 0x000F0000,
-  eciStandardReserved = 0x00100000,
-  eciStandardThai = 0x00110000,
-  eciStandardThaiTIS = eciStandardThai
-};
-
-//
 enum { ANNOTATION_MAX_SIZE = 10, LANG_INFO_MAX = 22 };
 
 struct langInfo {
@@ -99,41 +24,6 @@ struct langInfo {
 };
 
 enum { LANGUAGE_MAX_LABEL = 30 };  // max size of the label field
-
-// End of ECI declarations.
-
-// >
-// <decls and function prototypes
-
-/*
- * The following declarations are derived from the publically
- * available documentation for ViaVoice TTS outloud. --they are
- * placed here to obviate the need for having the ViaVoice SDK
- * installed.
- */
-
-typedef enum { eciDataNotProcessed, eciDataProcessed } ECICallbackReturn;
-
-typedef enum {
-  eciWaveformBuffer,
-  eciPhonemeBuffer,
-  eciIndexReply,
-  eciPhonemeIndexReply
-} ECIMessage;
-
-typedef enum {
-  eciSynthMode,
-  eciInputType,
-  eciTextMode,
-  eciDictionary,
-  eciSampleRate = 5,
-  eciWantPhonemeIndices = 7,
-  eciRealWorldUnits,
-  eciLanguageDialect,
-  eciNumberMode,
-  eciPhrasePrediction,
-  eciNumParams
-} ECIParam;
 
 class LangSwitcher;
 
